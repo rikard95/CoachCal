0fa357afe4aa166027c66aea7aefb116032b48f9
Rikard Nilsson <rikardnilsson95@gmail.com>
2025-12-05 13:12:43 +0100

    Initial project setup: Vite + React + TypeScript + Firebase + basic component skeleton

---
+++ coachcal/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>CoachCal</h1>
+      <p>Welcome to your project! Start building your components here.</p>
+    </div>
+  );
+}
+
+export default App;
